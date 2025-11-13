1bdb30f1d146e6cfc188788a05c9f1ba69528312
norinorinori_3 <phileo0221@gmail.com>
2025-11-13 15:38:43 +0700

    SUPABASE_URL_V2

--- backend/supabaseClient.ts
+++ backend/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 import 'dotenv/config';
 
-const supabaseUrl = process.env.SUPABASE_URL!;
-const supabaseKey = process.env.SUPABASE_KEY!;
+const supabaseUrl = process.env.SUPABASE_URL_V2!;
+const supabaseKey = process.env.SUPABASE_KEY_V2!;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
